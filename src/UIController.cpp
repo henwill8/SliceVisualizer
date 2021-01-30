2ddfe6c787e9647412431aa88bf47b77f6552d54
Henwill8 <henwill8@yahoo.com>
2021-01-30 12:55:12 -0700

    Initial Upload

---
+++ src/UIController.cpp
@@ -0,0 +1,47 @@
+#include "../include/UI.hpp"
+#include "../include/codegen.hpp"
+using namespace QuestUI;
+using namespace UnityEngine::UI;
+using namespace UnityEngine;
+
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    config.Load();
+    return config;
+}
+
+#define CreateIncrement(parent, floatConfigValue, name, decimals, increment, hasMin, hasMax, minValue, maxValue) QuestUI::BeatSaberUI::CreateIncrementSetting(parent, name, decimals, increment, floatConfigValue.GetFloat(), hasMin, hasMax, minValue, maxValue, UnityEngine::Vector2{}, il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<float>*>(classof(UnityEngine::Events::UnityAction_1<float>*), (void*)nullptr, +[](float value) { floatConfigValue.SetFloat(value); }))
+
+#define CreateToggle(parent, boolConfigValue, name) QuestUI::BeatSaberUI::CreateToggle(parent, name, boolConfigValue.GetBool(), il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction_1<bool>*>(classof(UnityEngine::Events::UnityAction_1<bool>*), (void*)nullptr, +[](bool value) { boolConfigValue.SetBool(value); }))
+
+DEFINE_CLASS(SliceVisualizer::UIController);
+
+void SliceVisualizer::UIController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+	if(firstActivation)
+	{
+		VerticalLayoutGroup* layout = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(get_rectTransform());
+		layout->set_spacing(1.5f);
+        
+        float spacing = 1.5f;
+		
+		VerticalLayoutGroup* layout1 = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(layout->get_rectTransform());
+		layout1->set_spacing(spacing);
+		layout1->get_gameObject()->AddComponent<QuestUI::Backgroundable*>()->ApplyBackground(il2cpp_utils::createcsstr("round-rect-panel"));
+
+        ContentSizeFitter* contentSizeFitter = layout1->get_gameObject()->AddComponent<ContentSizeFitter*>();
+		contentSizeFitter->set_horizontalFit(ContentSizeFitter::FitMode::PreferredSize);
+		contentSizeFitter->set_verticalFit(ContentSizeFitter::FitMode::PreferredSize);
+
+		layout1->set_padding(UnityEngine::RectOffset::New_ctor(3, 3, 2, 2));
+
+		Transform* Parent1 = layout1->get_transform();
+
+        CreateToggle(Parent1, getConfig().config["Enabled"], "Enabled");
+        QuestUI::BeatSaberUI::AddHoverHint(CreateToggle(Parent1, getConfig().config["DynamicFadeSpeed"], "Dynamic Fade Speed")->get_gameObject(), "When enabled, slice visuals will fade faster during faster parts of songs");
+        QuestUI::BeatSaberUI::AddHoverHint(CreateIncrement(Parent1, getConfig().config["FadeSpeed"], "Fade Speed", 0, 1, true, false, 0, 0.0f)->get_gameObject(), "The speed that slice visuals fade at, a higher value means it fades quicker");
+    }
+}
+
+void SliceVisualizer::UIController::DidDeactivate(bool removedFromHierarchy, bool systemScreenDisabling)  {
+    getConfig().Write();
+}
---
+++ src/main.cpp
@@ -0,0 +1,297 @@
+#include "../include/main.hpp"
+#include "../include/codegen.hpp"
+#include "../include/UI.hpp"
+#include "../include/sprites.hpp"
+#include "../extern/beatsaber-hook/shared/config/config-utils.hpp"
+#include "../extern/beatsaber-hook/shared/utils/utils.h"
+#include "../extern/beatsaber-hook/shared/utils/logging.hpp"
+#include "../extern/modloader/shared/modloader.hpp"
+#include "../extern/beatsaber-hook/shared/utils/typedefs.h"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+#include "../extern/beatsaber-hook/shared/config/rapidjson-utils.hpp"
+#include "../extern/beatsaber-hook/shared/config/config-utils.hpp"
+#include "../extern/questui/shared/QuestUI.hpp"
+#include "../extern/questui/shared/BeatSaberUI.hpp"
+#include "../extern/custom-types/shared/register.hpp"
+
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <math.h>
+#include <fstream>
+#include <string.h>
+
+using namespace UnityEngine::UI;
+using namespace il2cpp_utils;
+using namespace GlobalNamespace;
+
+static float clamp(float num, float min, float max) {
+    if(num < min) {
+        return min;
+    } else if(num > max) {
+        return max;
+    } else {
+        return num;
+    }
+}
+
+Logger& logger() {
+    static auto logger = new Logger(modInfo, LoggerOptions(false, false));
+    return *logger;
+}
+
+#define log(...) logger().info(__VA_ARGS__)
+
+static void SaveConfig() {
+    if (!getConfig().config.HasMember("DynamicFadeSpeed")) {
+        log("Regenerating config!");
+        getConfig().config.SetObject();
+        auto& allocator = getConfig().config.GetAllocator();
+        
+        getConfig().config.AddMember("Enabled", true, allocator);
+        getConfig().config.AddMember("FadeSpeed", 10, allocator);
+        getConfig().config.AddMember("DynamicFadeSpeed", true, allocator);
+
+        getConfig().Write();
+        log("Config regeneration complete!");
+    }
+    else {
+        log("Not regnerating config.");
+    }
+}
+
+template<class T>
+UnityEngine::GameObject* FindObject(std::string name, bool byParent = true, bool getLastIndex = false) {
+    log("Finding GameObject of name "+name);
+    Array<T>* trs = UnityEngine::Resources::FindObjectsOfTypeAll<T>();
+    log("There are "+std::to_string(trs->Length())+" GameObjects");
+    for(int i = 0; i < trs->Length(); i++) {
+        if(i != trs->Length()-1 && getLastIndex) continue;
+        UnityEngine::GameObject* go = trs->values[i]->get_gameObject();
+        if(byParent) {
+            go = go->get_transform()->GetParent()->get_gameObject();
+        }
+        log(to_utf8(csstrtostr(UnityEngine::Transform::GetName(trs->values[i]))));
+        if(to_utf8(csstrtostr(UnityEngine::Transform::GetName(go))) == name){
+            log("Found GameObject");
+            return go;
+        }
+    }
+    log("Could not find GameObject");
+    return nullptr;
+}
+
+void SetGlobalScale(UnityEngine::Transform* transform, UnityEngine::Vector3 globalScale) {
+    transform->set_localScale(UnityEngine::Vector3::get_one());
+    transform->set_localScale(UnityEngine::Vector3{globalScale.x/transform->get_lossyScale().x, globalScale.y/transform->get_lossyScale().y, globalScale.z/transform->get_lossyScale().z});
+}
+
+float distanceToCenter;
+
+UnityEngine::Sprite* arrowSprite;
+UnityEngine::Sprite* dotSprite;
+UnityEngine::Sprite* arrowBackgroundSprite;
+UnityEngine::Sprite* dotBackgroundSprite;
+
+UnityEngine::GameObject* sliceVisualizerGO;
+
+UnityEngine::Color leftColor;
+UnityEngine::Color rightColor;
+
+BeatmapObjectSpawnController* spawnController;
+
+std::vector<std::pair<std::pair<std::pair<UnityEngine::UI::Image*, UnityEngine::UI::Image*>, UnityEngine::UI::Image*>, float>> cuts;
+
+float nextNoteTime;
+
+float spriteSize = 0.6f;
+bool enabled = true;
+float fadeSpeed;
+bool dynamicFadeSpeed = true;
+bool cutHasBeenMade = false;
+
+void createSlice(NoteCutInfo* noteCutInfo, NoteController* noteController, float distanceToCenter) {
+    cutHasBeenMade = true;
+
+    if(spawnController == nullptr) {
+        spawnController = FindObject<BeatmapObjectSpawnController*>("BeatmapObjectSpawnController", false)->GetComponent<BeatmapObjectSpawnController*>();
+    }
+
+    // log("Making sprite Image");
+    UnityEngine::GameObject* sliceGraphics = UnityEngine::GameObject::New_ctor(createcsstr("SliceGraphics"));
+    sliceGraphics->get_transform()->SetParent(sliceVisualizerGO->get_transform(), false);
+    sliceGraphics->get_transform()->set_localEulerAngles(UnityEngine::Vector3{0, 0, NoteCutDirectionExtensions::RotationAngle(noteController->get_noteData()->get_cutDirection())});
+    UnityEngine::Vector3 position = sliceGraphics->get_transform()->get_position();
+    UnityEngine::Vector2 notePosition = spawnController->beatmapObjectSpawnMovementData->Get2DNoteOffset(noteController->noteData->lineIndex, noteController->noteData->noteLineLayer);
+    sliceGraphics->get_transform()->set_position(UnityEngine::Vector3{notePosition.x, position.y + notePosition.y, position.z});
+    sliceGraphics->get_transform()->set_localScale(UnityEngine::Vector3{spriteSize, spriteSize, spriteSize});
+
+    UnityEngine::GameObject* spriteGO = UnityEngine::GameObject::New_ctor(createcsstr("SpriteImage"));
+    spriteGO->get_transform()->SetParent(sliceGraphics->get_transform(), false);
+    spriteGO->get_transform()->set_localPosition(UnityEngine::Vector3{-1.65f/spriteSize, -1.65f/spriteSize, 0});
+    UnityEngine::UI::Image* sprite = spriteGO->AddComponent<UnityEngine::UI::Image*>();
+    ColorType colorType = noteController->noteData->colorType;
+    if(colorType == ColorType::ColorA) {
+        sprite->set_color(leftColor);
+    } else {
+        sprite->set_color(rightColor);
+    }
+    UnityEngine::UI::Mask* spriteMask = spriteGO->AddComponent<UnityEngine::UI::Mask*>();
+    spriteMask->set_showMaskGraphic(true);
+
+    UnityEngine::GameObject* spriteArrowGO = UnityEngine::GameObject::New_ctor(createcsstr("SpriteArrowImage"));
+    spriteArrowGO->get_transform()->SetParent(spriteGO->get_transform(), false);
+    spriteArrowGO->get_transform()->set_localPosition(UnityEngine::Vector3{0, 0, 0});
+    UnityEngine::UI::Image* spriteArrow = spriteArrowGO->AddComponent<UnityEngine::UI::Image*>();
+
+    if(noteController->noteData->cutDirection == NoteCutDirection::Any) {
+        sprite->set_sprite(dotBackgroundSprite);
+        spriteArrow->set_sprite(dotSprite);
+    } else {
+        sprite->set_sprite(arrowBackgroundSprite);
+        spriteArrow->set_sprite(arrowSprite);
+    }
+
+    // log("Creating line");
+    UnityEngine::GameObject* lineGO = UnityEngine::GameObject::New_ctor(createcsstr("CutLine"));
+    lineGO->get_transform()->SetParent(spriteGO->get_transform(), false);
+    lineGO->get_transform()->set_localScale(UnityEngine::Vector3{1.0f/spriteSize, 1.0f/spriteSize, 1.0f/spriteSize});
+    lineGO->AddComponent<UnityEngine::UI::Image*>()->set_color(UnityEngine::Color{0, 0, 0, 1});
+    UnityEngine::RectTransform* lineRT = lineGO->GetComponent<UnityEngine::RectTransform*>();
+    lineRT->set_sizeDelta(UnityEngine::Vector2{5, 300});
+    
+    lineGO->get_transform()->set_localEulerAngles(UnityEngine::Vector3{0, 0, noteCutInfo->cutDirDeviation});
+    lineGO->get_transform()->set_localPosition(UnityEngine::Vector3{distanceToCenter*120/spriteSize - 2.3f/spriteSize, -2.3f/spriteSize, 0});
+
+    // log("Adding slice to the cut vector");
+    cuts.push_back(std::make_pair(std::make_pair(std::make_pair(sprite, spriteArrow), lineGO->GetComponent<UnityEngine::UI::Image*>()), 0.99f));
+    // log("Successfully added the slice");
+}
+
+MAKE_HOOK_OFFSETLESS(SongUpdate, void, AudioTimeSyncController* self) {
+    if(cuts.size() > 0 && cutHasBeenMade) {
+        for(int i = cuts.size()-1; i >= 0; i--) {
+            float dynamicMultiplier = 1;
+            if(dynamicFadeSpeed) dynamicMultiplier = clamp(2 - (nextNoteTime * 1.3f), 0.4f, 2.0f);
+            float opacity = cuts[i].second - (std::min(0.4f, 1 - cuts[i].second) * UnityEngine::Time::get_deltaTime() * fadeSpeed * dynamicMultiplier);
+            if(cuts[i].first.first.first->m_CachedPtr.m_value != nullptr) {
+                UnityEngine::GameObject* go = cuts[i].first.first.first->get_gameObject();
+                
+                UnityEngine::Color color = cuts[i].first.first.first->get_color();
+                color.a = opacity;
+                cuts[i].first.first.first->set_color(color);
+                cuts[i].first.first.second->set_color(UnityEngine::Color{1, 1, 1, opacity});
+                cuts[i].first.second->set_color(UnityEngine::Color{0, 0, 0, opacity*2.2f});
+                cuts[i].second = opacity;
+                if(opacity < 0) {
+                    // log("Destroying slice as its opacity has passed 0");
+                    cuts.erase(cuts.begin()+i);
+                    UnityEngine::Object::Destroy(go->get_transform()->GetParent()->get_gameObject());
+                    continue;
+                }
+            }
+        }
+    }
+
+    SongUpdate(self);
+}
+
+MAKE_HOOK_OFFSETLESS(SongStart, void, AudioTimeSyncController* self, float startTimeOffset) {
+
+    SongStart(self, startTimeOffset);
+
+    enabled = getConfig().config["Enabled"].GetBool();
+    fadeSpeed = getConfig().config["FadeSpeed"].GetFloat();
+    dynamicFadeSpeed = getConfig().config["DynamicFadeSpeed"].GetBool();
+
+    cuts.clear();
+    cutHasBeenMade = false;
+
+    log("Getting arrow sprite");
+    arrowSprite = QuestUI::BeatSaberUI::Base64ToSprite(arrowBase64, 1024, 1024);
+    log("Getting dot sprite");
+    dotSprite = QuestUI::BeatSaberUI::Base64ToSprite(dotBase64, 1024, 1024);
+    log("Getting arrow background sprite");
+    arrowBackgroundSprite = QuestUI::BeatSaberUI::Base64ToSprite(arrowBackgroundBase64, 1024, 1024);
+    log("Getting dot background sprite");
+    dotBackgroundSprite = QuestUI::BeatSaberUI::Base64ToSprite(dotBackgroundBase64, 1024, 1024);
+
+    sliceVisualizerGO = UnityEngine::GameObject::New_ctor(createcsstr("SliceVisualizerGO"));
+    bool getLastIndex = false;
+    if(FindObject<MultiplayerController*>("MultiplayerController", false) == nullptr) {
+        getLastIndex = true;
+    }
+    log("Get last index is %i", getLastIndex);
+    sliceVisualizerGO->get_transform()->SetParent(FindObject<ComboUIController*>("ComboPanel", false, getLastIndex)->get_transform());
+    sliceVisualizerGO->get_transform()->set_position(UnityEngine::Vector3{0, 3, 15});
+    sliceVisualizerGO->get_transform()->set_eulerAngles(UnityEngine::Vector3{0, 0, 0});
+    SetGlobalScale(sliceVisualizerGO->get_transform(), UnityEngine::Vector3{0.01f, 0.01f, 0.01f});
+
+    spawnController = nullptr;
+}
+
+MAKE_HOOK_OFFSETLESS(NoteController_SendNoteWasCutEvent, void, NoteController* self, NoteCutInfo* noteCutInfo) {
+    
+    if(noteCutInfo->get_allIsOK() && enabled) {
+        if(dynamicFadeSpeed) {
+            nextNoteTime = self->noteData->timeToNextColorNote;
+        }
+        createSlice(noteCutInfo, self, distanceToCenter);
+    }
+
+    NoteController_SendNoteWasCutEvent(self, noteCutInfo);
+}
+
+MAKE_HOOK_OFFSETLESS(GameNoteController_HandleBigWasCutBySaber, void, GameNoteController* self, Il2CppObject* saber, UnityEngine::Vector3 cutPoint, UnityEngine::Quaternion orientation, Vector3 cutDirVec) {
+    
+    UnityEngine::Vector3 vector = orientation * UnityEngine::Vector3::get_up();
+    UnityEngine::Plane plane = {vector, cutPoint};
+    distanceToCenter = plane.GetDistanceToPoint(self->noteTransform->get_position());
+
+    GameNoteController_HandleBigWasCutBySaber(self, saber, cutPoint, orientation, cutDirVec);
+}
+
+MAKE_HOOK_OFFSETLESS(ColorManager_Start, void, ColorManager* self) {
+
+    log("ColorManager_Start");
+
+    ColorManager_Start(self);
+    
+    leftColor = self->ColorForSaberType(SaberType::SaberA);
+    rightColor = self->ColorForSaberType(SaberType::SaberB);
+
+    log("Got both colors successfully");
+}
+
+extern "C" void setup(ModInfo& info) {
+    info.id = "SliceVisualizer";
+    info.version = "0.1.0";
+    modInfo = info;
+    
+    getConfig().Load();
+    SaveConfig();
+}
+
+extern "C" void load() {
+    QuestUI::Init();
+
+    custom_types::Register::RegisterType<SliceVisualizer::UIController>();
+    QuestUI::Register::RegisterModSettingsViewController<SliceVisualizer::UIController*>(modInfo, "Slice Visualizer");
+
+    log("Installing hooks...");
+    INSTALL_HOOK_OFFSETLESS(logger().get(), SongUpdate, il2cpp_utils::FindMethodUnsafe("", "AudioTimeSyncController", "Update", 0));
+    INSTALL_HOOK_OFFSETLESS(logger().get(), SongStart, il2cpp_utils::FindMethodUnsafe("", "AudioTimeSyncController", "StartSong", 1));
+    INSTALL_HOOK_OFFSETLESS(logger().get(), NoteController_SendNoteWasCutEvent, il2cpp_utils::FindMethodUnsafe("", "NoteController", "SendNoteWasCutEvent", 1));
+    INSTALL_HOOK_OFFSETLESS(logger().get(), GameNoteController_HandleBigWasCutBySaber, il2cpp_utils::FindMethodUnsafe("", "GameNoteController", "HandleBigWasCutBySaber", 4));
+    INSTALL_HOOK_OFFSETLESS(logger().get(), ColorManager_Start, il2cpp_utils::FindMethodUnsafe("", "ColorManager", "Start", 0));
+    log("Installed all hooks!");
+}
